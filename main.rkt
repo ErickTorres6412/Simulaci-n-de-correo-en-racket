11186b85f5b124d523db80c3d384405888c1f4c2
ErickTorres6412 <erick.torres.hernandez@est.una.ac.cr>
2024-11-14 16:43:22 -0600

    ....

--- main.rkt
+++ main.rkt
@@ -5,13 +5,16 @@
          web-server/servlet-env
          "handlers.rkt")
 
+;; Definimos el puerto desde la variable de entorno 'PORT', con valor predeterminado 3000
+(define puerto (string->number (or (getenv "PORT") "8080")))
+
 ;; Iniciamos el servidor web
 (serve/servlet 
  start  ; La función 'start' está definida en handlers.rkt
  #:launch-browser? #f  ; No abrimos automáticamente el navegador
  #:quit? #f  ; El servidor no se detendrá automáticamente
- #:port 8080  ; El servidor escuchará en el puerto 8080
+ #:port puerto  ; El servidor escuchará en el puerto de la variable de entorno 'PORT'
  #:servlet-regexp #rx"")  ; Todas las rutas serán manejadas por nuestro servlet
 
 ;; Imprimimos un mensaje para indicar que el servidor está en funcionamiento
-(displayln "Servidor iniciado en http://localhost:8080")
+(displayln (string-append "Servidor iniciado en el puerto " (number->string puerto)))
